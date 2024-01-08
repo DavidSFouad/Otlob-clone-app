f6bd7449f35e21d5fd209c1c313a68bf0cc3bc63
DavidSFouad <davidsameh302@gmail.com>
2024-01-08 20:14:22 +0200

    new main @hamza

--- OtlobGUI.java
+++ OtlobGUI.java
@@ -1,25 +1,24 @@
-//Otlob-Clone-App
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
+import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import javafx.util.Duration;
 
 /*
 
@@ -29,23 +28,122 @@ author @Hamza Magdy
 
 public class OtlobGUI extends Application {
 
-  
+// role for choice admin/customer/seller IMPORTANT
+  private String selectedRole;
     
-    @Override
+  
+  @Override
     public void start(Stage mainstage)throws Exception{
-      // First login page (choosing between admin,seller and customer)  
-   Pane main = new Pane();
+
+//Login page
+
+        Label LoginWelcome = new Label("Welcome Back");
+        LoginWelcome.setStyle("-fx-text-fill: rgb(98, 42, 123)");
+        LoginWelcome.setFont(Font.font("Impact", FontWeight.SEMI_BOLD, 60));
+        LoginWelcome.setAlignment(Pos.TOP_CENTER);
+        LoginWelcome.setMinHeight(250);
+
+        VBox logSign = new VBox(1000);
+        logSign.setSpacing(7);
+        logSign.setAlignment(Pos.CENTER);
+
+        Scene LiSu = new Scene(logSign, 1000, 600);
+        Label LoginLabel = new Label("Enter Username:");
+
+        LoginLabel.setStyle("-fx-text-fill: rgb(98, 42, 123)");
+        LoginLabel.setFont(Font.font("Impact", FontWeight.SEMI_BOLD, 25));
+
+        TextField Logintype = new TextField();
+        
+        Logintype.setMaxSize(110, 110);
+        Label LoginPass = new Label("Enter Password:");
+        LoginPass.setStyle("-fx-text-fill: rgb(98, 42, 123)");
+        LoginPass.setFont(Font.font("Impact", FontWeight.SEMI_BOLD, 25));
+
+        Button goToSignup = new Button("Click here to SignUp");
+        goToSignup.setStyle("-fx-font-size: 14px; " + "-fx-underline: true;");
+        goToSignup.setFont(Font.font(STYLESHEET_MODENA));
+        goToSignup.setStyle("-fx-background-color: rgb(255, 222, 0);-fx-text-fill: rgb(98, 42, 123);-fx-border-color: rgb(98, 42, 123); -fx-border-width: 0px;");
+        
+        Button home = new Button("Go Back");
+        home.setStyle("-fx-background-color: rgb(255, 222, 0);-fx-text-fill: rgb(98, 42, 123);-fx-border-color: rgb(98, 42, 123); -fx-border-width: 1px;");
+        home.setAlignment(Pos.TOP_LEFT);
+        HBox lay = new HBox(home);
+
+        Button Login = new Button("Login");
+
+        TextField LoginEnter = new TextField();
+        LoginEnter.setMaxSize(110, 110);
+        logSign.getChildren().addAll(lay, LoginWelcome, LoginLabel, Logintype, LoginPass, LoginEnter, goToSignup, Login);
+        logSign.setStyle("-fx-background-color: rgb(255, 222, 0);");
+
+     
+
+     //Sign up scene
+     BorderPane signup = new BorderPane();
+     signup.setPrefSize(1000,600);
+     Scene signupscene = new Scene(signup,1000,600);
+     goToSignup.setOnAction(e->mainstage.setScene(signupscene));
+     
+     Label ere = new Label("Welcome !!");
+     ere.setStyle("-fx-text-fill: rgb(98, 42, 123)");
+      ere.setFont(Font.font("Impact",FontWeight.SEMI_BOLD,60));
+     ere.setMinSize(30, 30);
+    BorderPane.setAlignment(ere, Pos.TOP_CENTER);
+        signup.setTop(ere);
+     signup.setStyle("-fx-background-color: rgb(255, 222, 0);");
+     
+     Label enterusr = new Label("Enter Username: ");
+     TextField signupusr = new TextField();
+     Label enterpass = new Label("Enter Password: ");
+     TextField signuppass = new TextField();
+     
+    
+     enterpass.setStyle("-fx-text-fill: rgb(98, 42, 123)");
+     enterusr.setStyle("-fx-text-fill: rgb(98, 42, 123)");
+      enterusr.setFont(Font.font("Impact",FontWeight.SEMI_BOLD,30));
+      enterpass.setFont(Font.font("Impact",FontWeight.SEMI_BOLD,30));
+       
+      Button SignupB = new Button("Create Account");
+      
+      
+      signupusr.setMaxWidth(110);
+     
+      signuppass.setMaxWidth(110);  
+      
+      //go back button
+      Button back = new Button("Go Back");
+      signup.setTop(back);
+      signup.setStyle("-fx-background-color: rgb(255, 222, 0);");
+      back.setOnMouseClicked(e-> mainstage.setScene(LiSu));
+      back.setStyle("-fx-background-color: rgb(255, 222, 0);-fx-text-fill: rgb(98, 42, 123);-fx-border-color: rgb(98, 42, 123); -fx-border-width: 1px;");
+     
+      
+       Label crt = new Label("Create a new account: ");
+        crt.setStyle("-fx-text-fill: rgb(98, 42, 123)");
+        crt.setFont(Font.font("Impact", FontWeight.SEMI_BOLD, 30));
+        BorderPane.setAlignment(crt, Pos.TOP_CENTER);
+        
+      //allignment
+    VBox box = new VBox(20,crt,enterusr,signupusr,enterpass,signuppass,SignupB);
+    box.setSpacing(13);
+    box.setAlignment(Pos.CENTER); 
+        box.setPrefWidth(60);
+        signup.setCenter(box);
+   
+// Choice page (choosing between admin,seller and customer)  
+      Pane main = new Pane();
       main.setStyle("-fx-background-color: rgb(255, 222, 0);");
       
       Text Welcome = new Text("Welcome");
-        Welcome.setTranslateX(410); 
+        Welcome.setTranslateX(405); 
         Welcome.setTranslateY(150);  
         Welcome.setFont(Font.font("Impact",FontWeight.BOLD,50));
         Welcome.setFill(Color.rgb(98, 42, 123));
         
         
         Text Log = new Text("Login As:");
-        Log.setTranslateX(470); 
+        Log.setTranslateX(465); 
         Log.setTranslateY(250);
         Log.setFont(Font.font("Impact",FontWeight.SEMI_BOLD,20));
         Log.setFill(Color.rgb(98, 42, 123));
@@ -54,14 +152,7 @@ public class OtlobGUI extends Application {
       Button Admin = new Button("Admin");
       Button Seller = new Button("Seller");
       Button Customer = new Button("Customer");
-      Customer.setOnAction(e -> {
-        System.out.println("Button clicked!"); // Replace this with your desired action
-    });
-    Seller.setOnAction(e -> {
-      System.out.println("Button clicked!"); // Replace this with your desired action
-  });
-  
-
+      
       Admin.setPrefWidth(200);
       Admin.setPrefHeight(80);
       Seller.setPrefWidth(200);
@@ -75,8 +166,7 @@ public class OtlobGUI extends Application {
       Seller.setFont(Font.font("Impact", FontWeight.BOLD, 22));
       Customer.setFont(Font.font("Impact", FontWeight.BOLD, 22));
       
-      
-      
+   
       choice.setSpacing(50);
       choice.getChildren().addAll(Admin,Seller,Customer);
       choice.setAlignment(Pos.CENTER);
@@ -85,19 +175,172 @@ public class OtlobGUI extends Application {
       
       main.getChildren().addAll(choice,Welcome,Log);
   
-     
-   Scene LoginChoice = new Scene(main,1000,600);
-   
-  
-       
+      Scene LoginChoice = new Scene(main,1000,600);
       
+      
+ 
+       home.setOnMouseClicked(e-> mainstage.setScene(LoginChoice));
+      
+    //admin scene 
+      StackPane adminpane = new StackPane();
+      Scene adminscene = new Scene(adminpane,1000,600);
+      
+      Admin.setOnAction(e -> {
+    selectedRole = "Admin";
+    mainstage.setScene(LiSu);});
+      
+      adminpane.setStyle("-fx-background-color: rgb(255, 222, 0);");
+        //Home button to go to homepage(login page)
+        Button homepageA = new Button("Home Page");
+       homepageA.setStyle("-fx-background-color: rgb(255, 222, 0);-fx-text-fill: rgb(98, 42, 123);-fx-border-color: rgb(98, 42, 123); -fx-border-width: 1px;");
+       homepageA.setAlignment(Pos.TOP_LEFT);
+       homepageA.setOnAction(e -> mainstage.setScene(LoginChoice));
+      
+       Label adminwlcm = new Label("Welcome Admin !");
+       adminwlcm.setAlignment(Pos.TOP_CENTER);
+        adminwlcm.setStyle("-fx-text-fill: rgb(98, 42, 123)");
+        adminwlcm.setFont(Font.font("Impact", FontWeight.SEMI_BOLD, 30));
+
+        VBox adminContent = new VBox(adminwlcm);
+        VBox.setVgrow(adminContent, Priority.ALWAYS);
+        adminContent.setPadding(new Insets(0,0,0,400));
+
+        HBox topBar = new HBox(homepageA);
+        topBar.setAlignment(Pos.TOP_LEFT);
+        topBar.setPadding(new Insets(10));
+
+        adminpane.getChildren().addAll(adminContent, topBar);
+      
+       //seller scene
+       GridPane sellerpane = new GridPane();
+       Scene sellerscene = new Scene(sellerpane,1000,600);
+        
+       Seller.setOnAction(e -> {
+    selectedRole = "Seller";
+    mainstage.setScene(LiSu);});
+       
+       sellerpane.setStyle("-fx-background-color: rgb(255, 222, 0);");
+       //Home button to go to homepage(login page)
+        Button homepageS = new Button("Home Page");
+       homepageS.setStyle("-fx-background-color: rgb(255, 222, 0);-fx-text-fill: rgb(98, 42, 123);-fx-border-color: rgb(98, 42, 123); -fx-border-width: 1px;");
+       homepageS.setOnAction(e -> mainstage.setScene(LoginChoice));
+      
+       Label sellerwlcm = new Label("Welcome Seller !");
+        sellerwlcm.setStyle("-fx-text-fill: rgb(98, 42, 123)");
+        sellerwlcm.setFont(Font.font("Impact", FontWeight.SEMI_BOLD, 30));
+     VBox sellerContent = new VBox(sellerwlcm);
+        VBox.setVgrow(sellerContent, Priority.ALWAYS);
+        sellerContent.setPadding(new Insets(0,0,0,400));
+
+        HBox topBar2 = new HBox(homepageS);
+        topBar2.setAlignment(Pos.TOP_LEFT);
+        topBar2.setPadding(new Insets(10));
+
+        sellerpane.getChildren().addAll(sellerContent, topBar2);
+       
+    //customer scene
+     GridPane customerpane = new GridPane();
+       Scene customerscene = new Scene(customerpane,1000,600);
+        
+        Customer.setOnAction(e -> {
+    selectedRole = "Customer";
+    mainstage.setScene(LiSu);});
+        
+        customerpane.setStyle("-fx-background-color: rgb(255, 222, 0);");
+       //Home button to go to homepage(login page)
+        Button homepageC = new Button("Home Page");
+       homepageC.setStyle("-fx-background-color: rgb(255, 222, 0);-fx-text-fill: rgb(98, 42, 123);-fx-border-color: rgb(98, 42, 123); -fx-border-width: 1px;");
+       homepageC.setOnAction(e -> mainstage.setScene(LoginChoice));
+      
+       Label userwlcm = new Label("Welcome Customer !");
+
+        userwlcm.setStyle("-fx-text-fill: rgb(98, 42, 123)");
+        userwlcm.setFont(Font.font("Impact", FontWeight.SEMI_BOLD, 30));
+        VBox userContent = new VBox(userwlcm);
+        VBox.setVgrow(userContent, Priority.ALWAYS);
+        userContent.setPadding(new Insets(0,0,0,396));
+
+        HBox topBar3 = new HBox(homepageC);
+        topBar3.setAlignment(Pos.TOP_LEFT);
+        topBar3.setPadding(new Insets(10));
+
+        customerpane.getChildren().addAll(userContent, topBar3);
+   
+      
+      
+     
+//Login and signup events 
+
+Login.setOnMouseClicked(e -> {
+  
+    if (selectedRole != null) {
+        
+        switch (selectedRole) {
+            case "Admin":
+                mainstage.setScene(adminscene);
+                break;
+            case "Seller":
+                mainstage.setScene(sellerscene);
+                break;
+            case "Customer":
+                mainstage.setScene(customerscene);
+                break;
+            default:
+               
+                break;
+        }
+    } else {
+        
+         Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Role Selection");
+        alert.setHeaderText(null);
+        alert.setContentText("Please go back and select Admin/Seller/Customer.");
+        alert.showAndWait();
+    }
+});
+
+SignupB.setOnMouseClicked(e -> {
+    if (selectedRole != null) {
+       
+        switch (selectedRole) {
+            case "Admin":
+                mainstage.setScene(adminscene);
+                break;
+            case "Seller":
+                mainstage.setScene(sellerscene);
+                break;
+            case "Customer":
+                mainstage.setScene(customerscene);
+                break;
+            default:
+                // Handle unexpected case
+                break;
+        }
+    } else {
+        
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Role Selection");
+        alert.setHeaderText(null);
+        alert.setContentText("Please go back and select Admin/Seller/Customer.");
+        alert.showAndWait();
+    }
+});
+
+        
+       //icon setup + mainstage showing
+
        mainstage.setResizable(false);
        mainstage.setTitle("Otlob");
-       mainstage.setScene(LoginChoice);
+       mainstage.setScene(LoginChoice);// <---Always set to LoginChoice
        mainstage.show();
-}    
+    }  
+  
+    
+
     public static void main(String[] args) {
-    launch(args);
+    
+        launch(args);
+    
     }
    
 }
